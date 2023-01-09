820ff2ca7f5ac19c324801f1ea74df1738c8bb9e
José Galdámez <josegaldamez1991@gmail.com>
2023-01-08 23:37:13 -0600

    adding semantic ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.scss";
+import { Button } from "semantic-ui-react";
 
 function App() {
   return (
     <div>
       <h1>Hola musicfy</h1>
+      <Button primary>Click Here</Button>
+      <Button secondary>Click Here</Button>
     </div>
   );
 }
